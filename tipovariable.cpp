6b606715623d2207d3b1c4e40dcb9f0caca92f0a
Jose Blancas <92336331+josblax@users.noreply.github.com>
2024-09-26 19:41:02 -0600

    Create tipovariable.cpp

---
+++ tipovariable.cpp
@@ -0,0 +1,21 @@
+// ConsoleApplication9.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+//Bibliotecas
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // 1. Entero (int)
+    int numeroEntero = 42; // declaracion
+    cout << "Entero : "<< numeroEntero << endl;
+
+    // 2. Entero corto (short int) -32768 a 32767
+    short int numeroCorto = 32000;
+    cout << "Entero Corto : " << numeroCorto << endl;
+
+    // 3. Entero Largo (long int)
+    long long numeroLargo = 555613451723L;
+    cout << "Entero Largo : " << numeroLargo << endl;
+}
+
